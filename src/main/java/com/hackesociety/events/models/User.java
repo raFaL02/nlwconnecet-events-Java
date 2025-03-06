5bf584d97e35cc0b66b7a87095b102836eeb0841
Rafael de Lima <rafalimabcl@gmail.com>
2025-03-06 16:26:46 -0300

    feat: add user model

---
+++ src/main/java/com/hackesociety/events/models/User.java
@@ -0,0 +1,42 @@
+package com.hackesociety.events.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "user_name", length = 255, nullable = false)
+    private String userName;
+    @Column(name = "user_email", length = 255, nullable = false, unique = true)
+    private String userEmail;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
