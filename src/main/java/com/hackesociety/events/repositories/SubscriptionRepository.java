ccf3743b590b98a91fab0c2ace3842f47e8d430b
Rafael de Lima <rafalimabcl@gmail.com>
2025-03-14 14:55:46 -0300

    feat: add findByEventAndSubscriber method in SubscriptionRepository

--- src/main/java/com/hackesociety/events/repositories/SubscriptionRepository.java
+++ src/main/java/com/hackesociety/events/repositories/SubscriptionRepository.java
@@ -1,8 +1,10 @@
 package com.hackesociety.events.repositories;
 
+import com.hackesociety.events.models.Event;
 import com.hackesociety.events.models.Subscription;
+import com.hackesociety.events.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SubscriptionRepository extends CrudRepository<Subscription, Integer> {
-
+    public Subscription findByEventAndSubscriber(Event event, User user);
 }
