9276c526199af0b4cea24613fad5f15f8b00108a
Rafael de Lima <rafalimabcl@gmail.com>
2025-03-14 14:56:52 -0300

    fix: add @Column annotation to userId in User entity

--- src/main/java/com/hackesociety/events/models/User.java
+++ src/main/java/com/hackesociety/events/models/User.java
@@ -8,6 +8,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Integer id;
     @Column(name = "user_name", length = 255, nullable = false)
     private String name;
