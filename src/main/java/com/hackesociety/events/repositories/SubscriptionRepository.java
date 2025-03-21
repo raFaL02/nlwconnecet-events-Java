d902013b6bd97f921d11bf55ff197e5039bab810
Rafael de Lima <rafalimabcl@gmail.com>
2025-03-21 11:53:37 -0300

    feat: add generateRanking method in SubscriptionRepository

--- src/main/java/com/hackesociety/events/repositories/SubscriptionRepository.java
+++ src/main/java/com/hackesociety/events/repositories/SubscriptionRepository.java
@@ -1,10 +1,24 @@
 package com.hackesociety.events.repositories;
 
+import com.hackesociety.events.dto.SubscriptionRankingItem;
 import com.hackesociety.events.models.Event;
 import com.hackesociety.events.models.Subscription;
 import com.hackesociety.events.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SubscriptionRepository extends CrudRepository<Subscription, Integer> {
     public Subscription findByEventAndSubscriber(Event event, User user);
+
+    @Query(value = " select count(subscription_number) as quantidade, indication_user_id, user_name " +
+            " from tbl_subscription inner join tbl_user " +
+            " on tbl_subscription.indication_user_id = tbl_user.user_id " +
+            " where indication_user_id is not null " +
+            " and event_id = :eventId " +
+            " group by indication_user_id " +
+            " order by quantidade desc ", nativeQuery = true)
+    public List<SubscriptionRankingItem> generateRanking(@Param("eventId") Integer eventId);
 }
